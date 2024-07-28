490ce715f34bdb495d3487a0f2172f76f73fecf2
JuanGuimaraes <juandasmerces@outlook.com>
2024-07-28 14:32:28 -0300

    finalizando

---
+++ src/components/Produto/__tests__/Produto.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, screen } from '@testing-library/react'
+import Produto from '..'
+import { rendenizaComProvider } from '../../../utils/tests'
+
+const jogo = {
+  id: 2,
+  categoria: 'RPG',
+  imagem: '',
+  plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+  preco: 199.9,
+  precoAntigo: 299.9,
+  titulo: 'Hogwarts Legacy'
+}
+
+describe('Testes para o componente produto', () => {
+  test('Deve rendenizar coretamente', () => {
+    rendenizaComProvider(<Produto game={jogo} />)
+    expect(screen.getByText('Hogwarts Legacy')).toBeInTheDocument()
+  })
+
+  test('Deve adicionar um item ao carrinho', () => {
+    const { store } = rendenizaComProvider(<Produto game={jogo} />)
+    const botao = screen.getByTestId('btn-adicionar-produto')
+    fireEvent.click(botao)
+
+    expect(store.getState().carrinho.itens).toHaveLength(1)
+  })
+})
